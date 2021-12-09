e4389bcca9c70e693712e1ab58c3abbae52e05a0
Dayananda <dayamn123@gmail.com>
2021-12-09 23:26:40 +0530

    First commit

---
+++ src/test/java/vtg01/Test1.java
@@ -0,0 +1,13 @@
+package vtg01;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void samplemethod1()
+	{
+		System.out.println("print 1");
+		System.out.println("print 2");
+	}
+}
