40f6a698bfa034e3d43a1b7742b36ba40dfa99f9
shilp <shilp@LAPTOP-RFKFALFD>
2021-12-10 10:00:55 +0530

    added

--- src/test/java/vtg01/Test1.java
+++ src/test/java/vtg01/Test1.java
@@ -8,8 +8,8 @@ public class Test1
 	public void samplemethod1()
 	{
 		System.out.println("print 1");
-		System.out.println("print 2");
 		System.out.println("print 3");
+		System.out.println("print 2");
 
 	}
 }
